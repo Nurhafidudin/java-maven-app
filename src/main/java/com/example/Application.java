3ec8317bbc04dce36ab5132ccc6808ca51c7eed8
Nurhafidudin <114470995+Nurhafidudin@users.noreply.github.com>
2025-05-07 05:21:51 +0000

    Update Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -13,21 +13,6 @@ public class Application {
     {
         SpringApplication.run(Application.class, args);
     }
-package com.example;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import javax.annotation.PostConstruct;
-
-@SpringBootApplication
-public class Application {
-
-    public static void main(String[] args)
-    {
-        SpringApplication.run(Application.class, args);
-    }
 
     @PostConstruct
     public void init()
